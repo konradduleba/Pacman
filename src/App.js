6f6c8792e3c0850c40d6ba4354b35368e817a3ac
Konrad Duleba <kon.duleba@gmail.com>
2020-10-16 16:23:36 +0200

    Ready to build

--- src/App.js
+++ src/App.js
@@ -1,134 +1,149 @@
-import React, { useState, useEffect, useRef } from 'react';
-import './App.scss';
-
-import Pacman from './img/pacman.png';
+import React, { Component } from 'react';
+import PCMan from './img/pacman.png';
+import Ghost from './img/ghost1.png';
 import Wall from './img/wall.png';
-import Ghost1 from './img/ghost1.png';
-import Ghost2 from './img/ghost2.png';
 import Coin from './img/coin.png';
 import Bg from './img/bg.png';
+import './App.scss';
+import { Board } from './board'
 
+export default class Pacman extends Component {
 
+    state = {
+        pacman: {
+            y: 4,
+            x: 10
+        },
+        ghost: {
+            y: 1,
+            x: 1
+        }
+    }
 
-function App() {
-  const [up, setUp] = useState(2);
-  const [left, setLeft] = useState(3);
+    // wall - 1
+    // coin - 2
+    // bg - 3
+    // pacman - 4
+    // ghost - 5
 
-  const pacman = {
-    y: up,
-    x: left
-  }
+    points = 0;
+    ghostMove;
 
-  // wall - 1
-  // coin - 2
-  // bg - 3
-  // pacman - 4
-
-  const movePacman = event => {
-    const keyCode = event.keyCode;
-    console.log(keyCode);
-
-    if (keyCode === 37) {
-      console.log(board)
-      board[pacman.y][pacman.x - 1] = 3;
-      setLeft(pacman.x - 1);
-      console.log(board)
+    endGameAlert() {
+        window.location.reload();
+        alert('Koniec gry')
     }
 
 
-    // switch (keyCode) {
-    //   case 37:
-    //     setLeft(left - 1)
-    //     break;
-    //   case 38:
-    //     break;
-    //   case 39:
-    //     break;
-    //   case 40:
-    //     break;
-    //   default:
-    //     console.log(keyCode)
-  }
+    checkEngGame() {
+        const { pacman, ghost } = this.state;
 
-  useEffect(() => {
-    document.addEventListener("keydown", movePacman, false);
+        if (ghost.x === pacman.x) {
+            if (ghost.y === pacman.y) {
+                this.endGameAlert();
+            }
+        }
+    }
 
-    return () => {
-      document.removeEventListener("keydown", movePacman, false);
-    };
-  });
+    checkAvaibleCoins() {
+        let coinsTable = [];
+        Board.map(element => element.map(thing => coinsTable.push(thing)));
+        const avaibleCoins = coinsTable.filter(coin => coin === 2);
+        if (!avaibleCoins.length) return this.endGameAlert()
+    }
 
-  const board = [
-    [1, 1, 1, 1, 1, 1, 1],
-    [1, 2, 1, 2, 1, 2, 1],
-    [1, 2, 2, 4, 2, 2, 1],
-    [1, 2, 1, 2, 1, 2, 1],
-    [1, 1, 1, 1, 1, 1, 1]
-  ]
+    checkIfPlayerCanMove = (y, x, a, b = 0) => {
+        if (Board[y - a][x - b] !== 1) {
+            if (Board[y - a][x - b] === 2) this.points += 10;
+            Board[y][x] = 3;
+            Board[y - a][x - b] = 4;
+            this.setState(prevState => {
+                return {
+                    pacman: {
+                        y: prevState.pacman.y - a,
+                        x: prevState.pacman.x - b
+                    }
+                }
+            })
+        }
+        else return false
+    }
 
-  function renderBoard(number, element) {
-    if (number === 1) return <img src={Wall} alt='' />;
-    else if (number === 2) return <img src={Coin} alt='' />
-    else if (number === 3) return <img src={Bg} alt='' />
-    else if (number === 4) return <img src={Pacman} alt='' />
-    else return console.log('chuj')
-  }
+    checkIfGhostCanMove = (y, x, a, b) => {
+        if (Board[y - a][x - b] !== 1) {
+            Board[y][x] = 3;
+            Board[y - a][x - b] = 5;
+            this.setState(prevState => {
+                return {
+                    ghost: {
+                        y: prevState.ghost.y - a,
+                        x: prevState.ghost.x - b
+                    }
+                }
+            })
+        }
+    }
 
-  return (
-    <div className="game">
-      {board.map(element => element.map(thing => renderBoard(thing, element)))}
-    </div>
-  );
+    moveGhost() {
+        const { x, y } = this.state.ghost;
+        const plusOrMinus = Math.random() < 0.5 ? -1 : 1;
+        let a = Math.floor(Math.random() * 2);
+        let b;
+        if (a === 1) b = 0;
+        else b = 1 * plusOrMinus;
+
+        a *= plusOrMinus;
+
+        this.checkIfGhostCanMove(y, x, a, b);
+        this.checkEngGame();
+    }
+
+    movePacman = keyCode => {
+        const { x, y } = this.state.pacman;
+
+        if (keyCode === 38) this.checkIfPlayerCanMove(y, x, 1)
+        else if (keyCode === 40) this.checkIfPlayerCanMove(y, x, -1)
+        else if (keyCode === 37) this.checkIfPlayerCanMove(y, x, 0, 1)
+        else if (keyCode === 39) this.checkIfPlayerCanMove(y, x, 0, -1)
+        else return null
+
+        this.checkEngGame();
+        this.checkAvaibleCoins();
+    }
+
+    componentDidMount() {
+        document.addEventListener("keydown", e => this.movePacman(e.keyCode));
+        this.ghostMove = setInterval(() => {
+            this.moveGhost()
+        }, 300);
+    }
+
+    componentWillUnmount() {
+        document.removeEventListener("keydown", e => this.movePacman(e.keyCode));
+        clearInterval(this.ghostMove)
+    }
+
+    renderBoard = number => {
+        if (number === 1) return <img src={Wall} alt='' />;
+        else if (number === 2) return <img src={Coin} alt='' />
+        else if (number === 3) return <img src={Bg} alt='' />
+        else if (number === 4) return <img src={PCMan} alt='' />
+        else if (number === 5) return <img src={Ghost} alt='' />
+        else return null
+    }
+
+    renderGame() {
+        return Board.map(element => element.map(thing => this.renderBoard(thing)))
+    }
+
+    render() {
+        return (
+            <>
+                <div className='game'>
+                    {this.renderGame()}
+                </div>
+                <p className='result'>{this.points}</p>
+            </>
+        )
+    }
 }
-
-export default App;
-
-
-
-/* <img src={Pacman} />
-      <img src={Wall} />
-      <img src={Ghost1} />
-      <img src={Ghost2} />
-      <img src={Coin} />
-      <img src={Bg} /> */
-
-
-/*
- const movePacman = event => {
-    const keyCode = event.keyCode;
-    const pacmanPosition = pacmanRef.current.getBoundingClientRect();
-    const pacmanLeft = pacmanPosition.left;
-    const pacmanTop = pacmanPosition.top;
-
-    switch (keyCode) {
-      case 37:
-        if (checkMovePosibility(pacmanLeft - 50)) setLeft(pacmanLeft - 50);
-        break;
-      case 38:
-        if (checkMovePosibility(pacmanTop - 50)) setTop(pacmanTop - 50);
-        break;
-      case 39:
-        if (checkMovePosibility(pacmanLeft + 50)) setLeft(pacmanLeft + 50);
-        break;
-      case 40:
-        if (checkMovePosibility(pacmanTop + 50)) setTop(pacmanTop + 50);
-        break;
-      default:
-        console.log(keyCode)
-    }
-  }
-
-  useEffect(() => {
-        document.addEventListener("keydown", movePacman, false);
-
-    return () => {
-        document.removeEventListener("keydown", movePacman, false);
-    };
-  });
-  */
-
-/*
-useEffect(() => {
-        document.addEventListener("keydown", movePacman, false);
-});
-*/
--- src/index.js
+++ src/index.js
@@ -1,10 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import Pacman from './Pacman';
+import App from './App';
 
 ReactDOM.render(
   <React.StrictMode>
-    <Pacman />
+    <App />
   </React.StrictMode>,
   document.getElementById('root')
 );
